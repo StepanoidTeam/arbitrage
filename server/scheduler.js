f595a1a47a3b6216a459e6af17cc8b73d07fd24a
Volodymyr Myshko <volodymyrm@wix.com>
2018-10-20 02:16:07 +0300

    scheduler error handler + freeze

--- server/scheduler.js
+++ server/scheduler.js
@@ -2,9 +2,27 @@ function delayer(delay = 1000) {
   return new Promise(resolve => setTimeout(resolve, delay));
 }
 
+let errorCount = 0;
+
 async function* scheduler(asyncTask, delay = 1000) {
   while (true) {
-    let result = await asyncTask();
+    let result = await asyncTask().catch(error => {
+      console.warn(error);
+      errorCount++;
+      return null;
+    });
+
+    if (result === null) {
+      console.warn("error freeze");
+      await delayer(delay * errorCount);
+      continue;
+    }
+
+    if (errorCount > 10) {
+      console.warn("error limit reached");
+      break;
+    }
+
     console.log("shd", Date.now());
     yield result;
     await delayer(delay);
