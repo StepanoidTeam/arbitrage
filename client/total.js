c796cc34d2c50f87f0169570b4732811691b0fa2
Volodymyr Myshko <volodymyrm@wix.com>
2018-10-19 15:05:19 +0300

    lit-html templater added to render tables

--- client/total.js
+++ client/total.js
@@ -1,100 +1,91 @@
+import { html, render } from "https://unpkg.com/lit-html";
+
+function createTable(platform, data, type) {
+  let tableName = `${type.toUpperCase()}s (${platform})`;
+
+  var table = html`
+  <table class=${type}>
+    <thead><tr><th>${tableName}</th></tr></thead>
+    <tbody>
+    ${data.map(
+      order => html`<tr><td>${order.price}</td><td>${order.volume}</td></tr>`
+    )}
+    </tbody>
+  </table>`;
+
+  return table;
+}
+
 fetch("/api/binance")
   .then(data => data.json())
   .then(function(json4ik) {
-    function addDiv(divName) {
-      var divBinanceWrapper = document.createElement("div");
-      //divBinanceWrapper.style.backgroundColor = "blue";
-      divBinanceWrapper.innerHTML = divName;
-      document.body.appendChild(divBinanceWrapper);
+    function binanceMapper(order) {
+      return { price: order[0], volume: order[1] };
     }
 
-    function addTable(tableName, orderType, color) {
-      var c, r, t;
-      t = document.createElement("table");
-      t.innerHTML = "<tr><th>" + tableName + "<tr></th>";
-      t.style.backgroundColor = color;
-      t.style.cssFloat = "left";
-      t.style.marginLeft = "15px";
-
-      for (i = 0; i < 10; i++) {
-        r = t.insertRow(i + 1);
-        c = r.insertCell(0);
-        c.innerHTML = orderType[i][0];
-        c = r.insertCell(1);
-        c.innerHTML = orderType[i][1];
-      }
-
-      document.body.appendChild(t);
-    }
-
-    addTable("Asks (Binance)", json4ik.asks, "pink");
-    addTable("Bids (Binance)", json4ik.bids, "LightGreen");
+    render(
+      [
+        createTable(
+          "Binance",
+          json4ik.asks.slice(0, 10).map(binanceMapper),
+          "ask"
+        ),
+        createTable(
+          "Binance",
+          json4ik.bids.slice(0, 10).map(binanceMapper),
+          "bid"
+        )
+      ],
+      document.querySelector(".binance")
+    );
   });
 
 //////////Bitfinex////////////////////////
 fetch("/api/bitfinex")
   .then(data => data.json())
   .then(function(json4ik) {
-    console.log(json4ik.asks[0].price);
-    console.log(json4ik.asks[0].amount);
-
-    addTable("Asks (Bitfinex)", json4ik.asks, "pink");
-    addTable("Bids (Bitfinex)", json4ik.bids, "LightGreen");
-
-    function addTable(tableName, orderType, color) {
-      var c, r, t;
-      t = document.createElement("table");
-      t.innerHTML = "<tr><th>" + tableName + "<tr></th>";
-      t.style.backgroundColor = color;
-      t.style.cssFloat = "left";
-      t.style.marginLeft = "15px";
-
-      for (i = 0; i < 10; i++) {
-        r = t.insertRow(i + 1);
-        c = r.insertCell(0);
-        c.innerHTML = orderType[i].price;
-        c = r.insertCell(1);
-        c.innerHTML = orderType[i].amount;
-      }
-
-      document.body.appendChild(t);
+    function bitfinexMapper(order) {
+      return { price: order.price, volume: order.amount };
     }
+
+    render(
+      [
+        createTable(
+          "BitFinEx",
+          json4ik.asks.slice(0, 10).map(bitfinexMapper),
+          "ask"
+        ),
+        createTable(
+          "BitFinEx",
+          json4ik.bids.slice(0, 10).map(bitfinexMapper),
+          "bid"
+        )
+      ],
+      document.querySelector(".bitfinex")
+    );
   });
 
 //////////Bittrex////////////////////////
 fetch("/api/bittrex")
   .then(data => data.json())
   .then(function(json4ik) {
-    console.log(json4ik.result.buy[0].Rate);
-    console.log(json4ik.result.buy[0].Quantity);
-    //console.log(json4ik.result);
-
-    addTable("Asks (Bittrex)", json4ik.result.sell, "pink");
-    addTable("Bids (Bittrex)", json4ik.result.buy, "LightGreen");
-
-    addDiv(json4ik.result.buy);
-    function addDiv(orderType) {
-      div = document.createElement("div");
-      div.innerHTML = orderType[0].Rate;
-      document.body.appendChild(div);
+    function bittrexMapper(order) {
+      return { price: order.Rate, volume: order.Quantity };
     }
 
-    function addTable(tableName, orderType, color) {
-      var c, r, t;
-      t = document.createElement("table");
-      t.innerHTML = "<tr><th>" + tableName + "<tr></th>";
-      t.style.backgroundColor = color;
-      t.style.cssFloat = "left";
-      t.style.marginLeft = "15px";
-
-      for (i = 0; i < 10; i++) {
-        r = t.insertRow(i + 1);
-        c = r.insertCell(0);
-        c.innerHTML = orderType[i].Rate;
-        c = r.insertCell(1);
-        c.innerHTML = orderType[i].Quantity;
-      }
-
-      document.body.appendChild(t);
-    }
+    render(
+      [
+        createTable(
+          "Bittrex",
+          json4ik.result.sell.slice(0, 10).map(bittrexMapper),
+          "ask"
+        ),
+        createTable(
+          "Bittrex",
+          json4ik.result.buy.slice(0, 10).map(bittrexMapper),
+          "bid"
+        )
+      ],
+      document.querySelector(".bittrex")
+    );
   });
