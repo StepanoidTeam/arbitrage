752c0b679fa4a05073de168a108c1ff8db9f0aac
Volodymyr Myshko <volodymyrm@wix.com>
2018-10-21 14:30:20 +0300

    xrp pairs added

--- server/exchangeConfigs.js
+++ server/exchangeConfigs.js
@@ -1,10 +1,10 @@
-const LIMIT_ORDERS = 5;
+const LIMIT_ORDERS = 3;
 
 const binance = {
   name: "binance",
   url: "https://api.binance.com",
   query: "/api/v1/depth?symbol=",
-  pairs: ["BTCUSDT"],
+  pairs: ["BTCUSDT", "XRPUSDT"],
   mappers: {
     orderbook: data => ({
       bids: data.bids.slice(0, LIMIT_ORDERS),
@@ -18,7 +18,7 @@ const bittrex = {
   name: "bittrex",
   url: "https://bittrex.com",
   query: "/api/v1.1/public/getorderbook?type=both&market=",
-  pairs: ["USDT-BTC"],
+  pairs: ["USDT-BTC", "USDT-XRP"],
   mappers: {
     orderbook: data => ({
       bids: data.result.buy.slice(0, LIMIT_ORDERS),
@@ -32,7 +32,7 @@ const bitfinex = {
   name: "bitfinex",
   url: "https://api.bitfinex.com",
   query: "/v1/book/",
-  pairs: ["btcusd"],
+  pairs: ["btcusd", "xrpusd"],
   mappers: {
     orderbook: data => ({
       bids: data.bids.slice(0, LIMIT_ORDERS),
@@ -47,3 +47,35 @@ module.exports = {
   bittrex,
   bitfinex
 };
+
+//todo: pairs to add
+// BTC-USDT
+// BTC-ETH
+// ETH-USDT
+// BTC-EOS
+// ETH-EOS
+// EOS-USDT
+
+// FEES:
+// -Bittrex - 0.25% / trade
+
+// -Bitfinex
+// 0.1% - Maker Fees/0.2% - Taker Fees
+
+// -Binance
+// 0.1000% - Maker/0.1000% - Taker (0.075/0.075, если юзается BNB)
+
+// -Poloniex
+// 0.1% - Maker Fees/0.2% - Taker Fees
+
+// -OKex
+// 0.1% - Maker Fees/0.15% - Taker Fees
+
+// -Huobi
+// 0.2% - Maker Fees/0.2% - Taker Fees
+
+// -HitBTC
+// +0.01% - Maker Fees/0.1% - Taker Fees
+
+// -BitForex
+// 0% - Maker Fees/0.05% - Taker Fees
