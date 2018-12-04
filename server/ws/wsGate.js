45a260bbd102814a3bd8bdeb481a840160552ebb
bobanko <mishko.vladimir@gmail.com>
2018-12-04 20:23:30 +0200

    gate ws reconnect

--- server/ws/wsGate.js
+++ server/ws/wsGate.js
@@ -16,12 +16,8 @@ const {
 function getSourceForPairs(globalPairs = []) {
   let pairs = getLocalPairs(globalPairs, exConfig);
 
-  const wsUrl = "wss://ws.gate.io/v3/";
-
-  const ws = new WebSocket(wsUrl);
   const subject = new Subject();
-
-  //const channel2pairMapping = {};
+  const wsUrl = "wss://ws.gate.io/v3/";
 
   function subscribe(ws) {
     let msg = {
@@ -33,62 +29,70 @@ function getSourceForPairs(globalPairs = []) {
     ws.send(JSON.stringify(msg));
   }
 
-  const orderbook = {};
+  function connect() {
+    const ws = new WebSocket(wsUrl);
 
-  function handle(params) {
-    let [, data, localPair] = params;
+    const orderbook = {};
 
-    let { globalPair } = pairs.find(p => p.localPair === localPair);
+    function handle(params) {
+      let [, data, localPair] = params;
 
-    orderbook[globalPair] = {
-      bid: data.bids ? last(data.bids) : orderbook[globalPair].bid,
-      ask: data.asks ? last(data.asks) : orderbook[globalPair].ask,
-    };
+      let { globalPair } = pairs.find(p => p.localPair === localPair);
 
-    const arrToPriceVolume = ([price, volume]) => ({
-      price,
-      volume,
+      orderbook[globalPair] = {
+        bid: data.bids ? last(data.bids) : orderbook[globalPair].bid,
+        ask: data.asks ? last(data.asks) : orderbook[globalPair].ask,
+      };
+
+      const arrToPriceVolume = ([price, volume]) => ({
+        price,
+        volume,
+      });
+
+      let bid = arrToPriceVolume(orderbook[globalPair].bid.map(x => +x));
+      let ask = arrToPriceVolume(orderbook[globalPair].ask.map(x => +x));
+
+      const bookTop = {
+        exName: exConfig.name,
+        pair: globalPair,
+        bid,
+        ask,
+      };
+
+      subject.next(bookTop);
+    }
+
+    ws.on("open", () => {
+      console.log(`${exConfig.name} connected:`, wsUrl);
+      subscribe(ws);
     });
 
-    let bid = arrToPriceVolume(orderbook[globalPair].bid.map(x => +x));
-    let ask = arrToPriceVolume(orderbook[globalPair].ask.map(x => +x));
-
-    const bookTop = {
-      exName: exConfig.name,
-      pair: globalPair,
-      bid,
-      ask,
+    ws.onclose = () => {
+      logger.disconnect(exConfig);
+      //todo: reconnect!
+      setTimeout(() => connect(), 10 * 1000);
     };
 
-    subject.next(bookTop);
+    ws.onerror = err => {
+      console.log(`⛔️   ${exConfig.name} error ${err}`);
+    };
+
+    ws.on("message", data => {
+      let msg = JSON.parse(data);
+
+      switch (msg.method) {
+        case "depth.update":
+          handle(msg.params);
+          break;
+        default:
+          console.log(`❓`, msg);
+          break;
+      }
+    });
   }
 
-  ws.on("open", () => {
-    console.log(`${exConfig.name} connected:`, wsUrl);
-    subscribe(ws);
-  });
-
-  //todo: reconnect!
-  ws.onclose = () => {
-    logger.disconnect(exConfig);
-  };
-
-  ws.onerror = err => {
-    console.log(`⛔️   ${exConfig.name} error ${err}`);
-  };
-
-  ws.on("message", data => {
-    let msg = JSON.parse(data);
-
-    switch (msg.method) {
-      case "depth.update":
-        handle(msg.params);
-        break;
-      default:
-        console.log(`❓`, msg);
-        break;
-    }
-  });
+  setImmediate(() => connect());
+  //const channel2pairMapping = {};
 
   return subject;
 }
