1839b06887ba51d0d8bb2866a3a7c51e5539bbf6
Dmytro Lyalyuk <norbhere@gmail.com>
2018-12-06 15:27:19 +0200

    bitfinex reconnect

--- server/ws/wsBitfinex.js
+++ server/ws/wsBitfinex.js
@@ -11,87 +11,90 @@ const {
 } = require("../configs");
 
 function getSourceForPairs(globalPairs = []) {
-  let pairs = getLocalPairs(globalPairs, exConfig);
+  const pairs = getLocalPairs(globalPairs, exConfig);
   //limitation just because bitfinex
   // gives different response when sub to 1 or 2-n pairs
   if (pairs.length < 2)
     throw new Error(`${exConfig.name} ws works with 2 pairs minimum, sorry`);
 
   const subject = new Subject();
-
-  //todo: rx-ify this
-  let orderBooks = {};
   const depth = 5;
-  const ws = new WebSocket("wss://api.bitfinex.com/ws/2");
 
-  //todo: reconnect!
-  ws.onclose = () => {
-    logger.disconnect(exConfig);
-  };
+  function connect() {
+    //todo: rx-ify this
+    let orderBooks = {};
 
-  ws.on("open", () => {
-    console.log(`${exConfig.name} - connected`);
+    const ws = new WebSocket("wss://api.bitfinex.com/ws/2");
 
-    let pairsSubscribed = [];
-    pairs
-      .map(({ localPair: symbol }) => ({
-        event: "subscribe",
-        channel: "book",
-        symbol,
-      }))
-      .forEach(msg => {
-        ws.send(JSON.stringify(msg));
-        pairsSubscribed.push(msg.symbol);
-      });
-
-    //todo: do these when subscribe ok received from ws instead - as at Huobi
-    console.log(
-      `✅  ${exConfig.name} - subscribed (tried) to ${
-        pairsSubscribed.length
-      } pairs: ${pairsSubscribed.join(", ").substr(0, 100)}`
-    );
-  });
-
-  function produceBook(orderBook, localPair) {
-    const ob = Array.from(orderBook).map(x => x[1]);
-    const bids = sortBy(ob.filter(x => x[2] > 0), [x => x[0]]).reverse();
-    const asks = sortBy(ob.filter(x => x[2] < 0), x => x[0]);
-
-    let { globalPair } = pairs.find(p => p.localPair === localPair);
-
-    const bookTop = {
-      exName: exConfig.name,
-      pair: globalPair,
-      bid: bids
-        .slice(0, depth)
-        .map(([price, count, volume]) => ({ price, volume }))
-        .shift(),
-      ask: asks
-        .slice(0, depth)
-        .map(([price, count, volume]) => ({ price, volume: -volume }))
-        .shift(),
+    ws.onclose = () => {
+      logger.disconnect(exConfig);
+      //todo: reconnect!
+      setTimeout(() => connect(), 3000);
     };
 
-    //todo: bitfinex loosing bid sometimes - investigate this shit!
-    if (!bookTop.bid) {
-      console.log(`💩  ${exConfig.name} bid lost!`);
-      console.log(bookTop);
-      console.log(orderBook);
-      console.log(ob);
+    ws.on("open", () => {
+      console.log(`${exConfig.name} - connected`);
+
+      let pairsSubscribed = [];
+      pairs
+        .map(({ localPair: symbol }) => ({
+          event: "subscribe",
+          channel: "book",
+          symbol,
+        }))
+        .forEach(msg => {
+          ws.send(JSON.stringify(msg));
+          pairsSubscribed.push(msg.symbol);
+        });
+
+      //todo: do these when subscribe ok received from ws instead - as at Huobi
+      console.log(
+        `✅  ${exConfig.name} - subscribed (tried) to ${
+          pairsSubscribed.length
+        } pairs: ${pairsSubscribed.join(", ").substr(0, 100)}`
+      );
+    });
+
+    function produceBook(orderBook, localPair) {
+      const ob = Array.from(orderBook).map(x => x[1]);
+      const bids = sortBy(ob.filter(x => x[2] > 0), [x => x[0]]).reverse();
+      const asks = sortBy(ob.filter(x => x[2] < 0), x => x[0]);
+
+      let { globalPair } = pairs.find(p => p.localPair === localPair);
+
+      const bookTop = {
+        exName: exConfig.name,
+        pair: globalPair,
+        bid: bids
+          .slice(0, depth)
+          .map(([price, count, volume]) => ({ price, volume }))
+          .shift(),
+        ask: asks
+          .slice(0, depth)
+          .map(([price, count, volume]) => ({ price, volume: -volume }))
+          .shift(),
+      };
+
+      //todo: bitfinex loosing bid sometimes - investigate this shit!
+      if (!bookTop.bid || !bookTop.ask) {
+        console.log(`💩  ${exConfig.name} bid/ask lost!`);
+        console.log(bookTop);
+        console.log(orderBook);
+        console.log(ob);
+      }
+
+      subject.next(bookTop);
+      return bookTop;
     }
 
-    subject.next(bookTop);
-    return bookTop;
-  }
+    let source = fromEvent(ws, "message").pipe(
+      map(event => event.data),
+      map(data => JSON.parse(data))
+    );
 
-  let source = fromEvent(ws, "message").pipe(
-    map(event => event.data),
-    map(data => JSON.parse(data))
-  );
+    const pairStreams = [];
 
-  const pairStreams = [];
-
-  /*
+    /*
   //connect
   { event: 'subscribed',
     channel: 'book',
@@ -101,67 +104,67 @@ function getSourceForPairs(globalPairs = []) {
     freq: 'F0',
     len: '25',
     pair: 'XRPUSD' }
- */
-  //save opened streams
-  source
-    .pipe(
-      filter(data => data.event === "subscribed"),
-      map(({ chanId, pair }) => ({
-        chanId,
-        pair,
-      }))
-    )
-    .subscribe(stream => pairStreams.push(stream));
-
-  /* 
-  
-  //update
-  [ 1220186,
-    [ [ 0.45861, 2, 1591.47202851 ],...]
   */
-  //map orderbooks to opened streams
-  source
-    .pipe(
-      filter(data => Array.isArray(data)),
-      map(([chanId, orderBook]) => {
-        let stream = pairStreams.find(stream => stream.chanId === chanId);
-        // console.log("@stream", stream);
-        return {
-          pair: stream.pair,
-          orderBook,
-        };
-      })
-    )
-    .subscribe(data => onWsMessage(data));
+    //save opened streams
+    source
+      .pipe(
+        filter(data => data.event === "subscribed"),
+        map(({ chanId, pair }) => ({
+          chanId,
+          pair,
+        }))
+      )
+      .subscribe(stream => pairStreams.push(stream));
 
-  function onWsMessage({ pair, orderBook }) {
-    if (Array.isArray(orderBook[0])) {
-      initBook(orderBook, pair);
-    } else {
-      updateBook(orderBook, pair);
+    /* 
+    //update
+    [ 1220186,
+      [ [ 0.45861, 2, 1591.47202851 ],...]
+  */
+    //map orderbooks to opened streams
+    source
+      .pipe(
+        filter(data => Array.isArray(data)),
+        map(([chanId, orderBook]) => {
+          let stream = pairStreams.find(stream => stream.chanId === chanId);
+          // console.log("@stream", stream);
+          return {
+            pair: stream.pair,
+            orderBook,
+          };
+        })
+      )
+      .subscribe(data => onWsMessage(data));
+
+    function onWsMessage({ pair, orderBook }) {
+      if (Array.isArray(orderBook[0])) {
+        initBook(orderBook, pair);
+      } else {
+        updateBook(orderBook, pair);
+      }
+    }
+
+    function initBook(orderBook, pair) {
+      const mapOrder = data => [data[0], data];
+      orderBooks[pair] = new Map(orderBook.map(mapOrder));
+    }
+
+    function updateBook(data, pair) {
+      let [price, count, amount] = data;
+
+      if (count > 0) {
+        //add or update
+        orderBooks[pair].set(price, [price, count, amount]);
+      } else {
+        //delete
+        orderBooks[pair].delete(price);
+      }
+
+      produceBook(orderBooks[pair], pair);
     }
   }
 
-  function initBook(orderBook, pair) {
-    const mapOrder = data => [data[0], data];
-    orderBooks[pair] = new Map(orderBook.map(mapOrder));
-  }
-
-  function updateBook(data, pair) {
-    let [price, count, amount] = data;
-
-    if (count > 0) {
-      //add or update
-      orderBooks[pair].set(price, [price, count, amount]);
-    } else {
-      //delete
-      orderBooks[pair].delete(price);
-    }
-
-    produceBook(orderBooks[pair], pair);
-  }
-
-  //source.subscribe(data => console.log(data));
+  setImmediate(() => connect());
 
   return subject;
 }
