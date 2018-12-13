5718d06015e2a25748c4aa4de8a2f3ed9cb71191
Volodymyr Myshko <myshko@myshko-macbookpro.roam.corp.google.com>
2018-12-14 00:21:07 +0200

    make logs dir when dblogger starts

--- server/helpers/dbLogger.js
+++ server/helpers/dbLogger.js
@@ -1,6 +1,9 @@
 const nosql = require("nosql");
+
+const { makeDir } = require("./file");
 // viewer - https://nosql.totaljs.com/
 function dbLogger(logName) {
+  makeDir(`./logs`);
   var DB = nosql.load(`./logs/log.${logName}.nosql`);
 
   return data => DB.insert(data);
