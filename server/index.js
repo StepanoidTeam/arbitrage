c7fc2daecc667f18be5c8f9431a3ee4d572e56b8
Volodymyr Myshko <myshko@myshko-macbookpro.roam.corp.google.com>
2018-12-14 16:28:50 +0200

    serve client logic extracted

--- server/index.js
+++ server/index.js
@@ -1,18 +1,2 @@
-//todo: server for future
-
-const express = require("express");
-const port = process.env.PORT || 3000;
-
-const app = express();
-//serve client files
-app.use(express.static("./client"));
-
-//all
-app.get("/api/all", async (req, res) => {
-  res.send(await "somedata");
-});
-
-//start server
-app.listen(port, () => {
-  console.log("🌐  [server] started");
-});
+//host client
+require("./serve-client");
