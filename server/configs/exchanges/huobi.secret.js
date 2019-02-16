fa2dcfa35c6479d3d93897fa410f913efe71243b
bobanko <mishko.vladimir@gmail.com>
2019-02-16 20:26:31 +0200

    empty secret configs for huobi and okex

---
+++ server/configs/exchanges/huobi.secret.js
@@ -0,0 +1,15 @@
+/**
+ * ⛔️ ATTENTION ⚠️
+ * ⛔️ DO NOT COMMIT THIS FILE
+ * ⛔️ SENSITIVE INFORMATION HERE
+ */
+
+const secret = {
+  apiKey: "",
+  secretKey: "",
+  passphrase: "",
+};
+
+console.warn("⚠️ WARNING: HUOBI keys not specified");
+
+module.exports = { secret };
---
+++ server/configs/exchanges/okex.secret.js
@@ -0,0 +1,15 @@
+/**
+ * ⛔️ ATTENTION ⚠️
+ * ⛔️ DO NOT COMMIT THIS FILE
+ * ⛔️ SENSITIVE INFORMATION HERE
+ */
+
+const secret = {
+  apiKey: "",
+  secretKey: "",
+  passphrase: "",
+};
+
+console.warn("⚠️ WARNING: OKEX keys not specified");
+
+module.exports = { secret };
