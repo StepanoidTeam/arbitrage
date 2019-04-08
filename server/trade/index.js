666c74812b1cdbf6a73c73d377e0e3bbd174c099
bobanko <mishko.vladimir@gmail.com>
2019-04-08 12:08:25 +0300

    trade api config mock added - in progress

--- server/trade/index.js
+++ server/trade/index.js
@@ -1,3 +1,30 @@
+const tradeConfig = {
+  binance: {
+    USDT: {
+      balance: 10,
+      minProfitPt: 0.5, //%
+      lowBalance: 600, //$
+      lowBalanceMinProfit: 0.2, //%
+    },
+    BTC: {
+      balance: 10,
+      minProfitPt: 0.5, //%
+      lowBalance: 0.15, //btc
+    },
+  },
+  gate: {},
+};
+
+/**
+ * после каждой произведенной сделки
+ * сравниваем баланс монеты на бирже
+ * (переменная balanceAmount) с lowBalanceXXX.
+ * Если баланс на бирже ниже $600, //?
+ * то меняем minProfitPt до 0.2%
+ * или вообще до 0.001% каких-нибудь
+ * до 0% короч
+ */
+
 const binance = require("./binance");
 const gate = require("./gate");
 const hitbtc = require("./hitbtc");
