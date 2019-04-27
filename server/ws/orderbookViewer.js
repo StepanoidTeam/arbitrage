a57633d74758ec03f52c1aa8ad32bf7d8c68dfca
bobanko <mishko.vladimir@gmail.com>
2019-04-28 02:10:03 +0300

    orderBookViewer can select from log files

--- server/ws/orderbookViewer.js
+++ server/ws/orderbookViewer.js
@@ -1,66 +1,71 @@
-const { navigateUpDown } = require("./navigateUpDown");
+const { selectFromDir } = require("./selectFromDir");
 const { checkBrokenBook } = require("./checkBrokenBook");
 const { readFileLineStream } = require("../helpers/readFileLineStream");
+const { consoleReducer } = require("./consoleReducer");
 
-const stats = {
-  ok: 0,
-  broken: 0,
-  total: 0,
-  current: 0,
-};
-const books = [];
+(async () => {
+  const stats = {
+    ok: 0,
+    broken: 0,
+    total: 0,
+    current: 0,
+  };
+  const books = [];
 
-readFileLineStream(
-  "logs/log.orderbook-test.bittrex.1556292501580.nosql" //btc mine
-  //"logs/log.orderbook-test.bittrex_BAT.1556313842451.nosql" //bat norb
-  //"logs/log.orderbook-test.bittrex.1556315220867.nosql" //dash norb
-).subscribe(
-  data => {
-    data = JSON.parse(data);
-    if (checkBrokenBook(data)) {
-      stats.broken++;
-    } else {
-      stats.ok++;
-    }
-    stats.total++;
-    books.push(data);
-  },
-  () => {
-    //error
-  },
-  () => {
-    //done
-    console.log(stats);
-    console.log("navigate using arrows (ctrl-c to exit)");
+  const filePath = await selectFromDir("logs", /log\.orderbook(.+)\.nosql$/);
 
-    (async () => {
-      let key;
-      while (
-        ((key = await navigateUpDown().catch(e => {
-          console.log("stopped");
-          return false;
-        })),
-        key)
-      ) {
-        switch (key) {
-          case "up": {
-            stats.current--;
-            break;
-          }
-          case "down": {
-            stats.current++;
-            break;
-          }
-        }
-        if (stats.current < 0) stats.current = stats.total - 1;
-        if (stats.current >= stats.total) stats.current = 0;
+  readFileLineStream(filePath).subscribe(
+    data => {
+      data = JSON.parse(data);
+      if (checkBrokenBook(data)) {
+        stats.broken++;
+      } else {
+        stats.ok++;
+      }
+      stats.total++;
+      books.push(data);
+    },
+    () => {
+      //error
+    },
+    navigateOrderbooks
+  );
+
+  async function navigateOrderbooks() {
+    await consoleReducer({
+      initialState: stats,
+      callback: state => {
         console.clear();
-        const book = books[stats.current];
+
+        const book = books[state.current];
         const bookState = !checkBrokenBook(book);
-        console.log(`📒   ${stats.current} / ${stats.total}`);
+        console.log("navigate using arrows (ctrl-c to exit)");
+        console.log(state);
         console.log(bookState ? "✅   ok" : "🅱️   broken");
         console.log(book);
-      }
-    })();
+      },
+      keyBindings: [
+        {
+          key: "up",
+          callback: (state, resolve) => {
+            state.current++;
+            if (state.current >= state.total) state.current = 0;
+          },
+        },
+        {
+          key: "down",
+          callback: (state, resolve) => {
+            state.current--;
+            if (state.current < 0) state.current = state.total - 1;
+          },
+        },
+        {
+          key: "return",
+          callback: (state, resolve) => {
+            resolve(null);
+          },
+        },
+      ],
+    });
   }
-);
+})();
